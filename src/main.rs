ab7ef1a979e90eaa91fd86e5a5b1d4e85b65f1b8
Plsr <christianpoplawski@gmail.com>
2022-02-11 20:47:14 +0100

    Check player wall collision

--- src/main.rs
+++ src/main.rs
@@ -82,10 +82,14 @@ fn new_map() -> Vec<TileType> {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destinaiton_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destinaiton_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
